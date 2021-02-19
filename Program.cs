5ab93708b3ef855c3b8cd165e83a67085da7a7e2
jirlinge <julienirlinger@gmail.com>
2021-02-19 10:30:21 +0100

    version corigé

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result=0;
+            Console.WriteLine("Hello World!");
+            List<int> numbers = new List<int> { 1, 3, 5, 7, 11 };
+            int indexnb;
+            Console.WriteLine($"la somme de {numbers[0]}+{numbers[1]}+{numbers[2]}+{numbers[3]}+{numbers[4]}");
+            for (indexnb = 0; indexnb < numbers.Count; indexnb++)
+                
+            {
+                result += numbers[indexnb];
+                
+               
+            }
+            Console.WriteLine($"est {result}");
+        }
+    }
+}
